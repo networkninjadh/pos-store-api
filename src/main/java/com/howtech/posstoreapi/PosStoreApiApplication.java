7306751e5f8b515e08a506657376d25ba2d6ffcf
networkninjadh <networkninjadh@gmail.com>
2022-04-12 12:20:24 -0500

    initital commit

---
+++ src/main/java/com/howtech/posstoreapi/PosStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.howtech.posstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/howtech/posstoreapi/PosStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howtech.posstoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
